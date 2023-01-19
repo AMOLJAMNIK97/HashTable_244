d41e410a39efc8e956ff1db586913612bb570b20
Amol <ajamnik.mech@gmail.com>
2023-01-19 22:07:50 +0530

    [Amol]- Find Frequency of the world large Hash Map

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -20,7 +20,19 @@ namespace HashTable
             hash.Add("4", "To");
             hash.Add("5", "be");
             string hash5=hash.Get("5");
+            string hash2=hash.Get("2");
+            string hash4=hash.Get("4");
+            string hash3=hash.Get("3");
+            string hash1=hash.Get("1");
+            string hash0=hash.Get("0");
+             
+             
             Console.WriteLine("5th index value: "+ hash5);
+            Console.WriteLine("2th index value: "+ hash2);
+            Console.WriteLine("4th index value: "+ hash4);
+            Console.WriteLine("3th index value: "+ hash3);
+            Console.WriteLine("1th index value: "+ hash1);
+            Console.WriteLine("0th index value: "+ hash0);
 
         }
     }
